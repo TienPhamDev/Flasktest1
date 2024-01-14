f40bbcaccc4917074e2a6bdbd891d3ad0f15214d
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2024-01-14 06:33:38 +0000

    learn template in flask

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/') # this is the home page route
 def index():
-    return 'Hello from Flask!'
+  #code
+  user_logged_in = True
+  return render_template('basic.html',user_logged_in = user_logged_in)
 
-
-app.run(host='0.0.0.0', port=81)
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=81,debug=True)
