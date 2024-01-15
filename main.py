aea0b7cbb40ffa0e61375b714ee17e4aff766731
TienPhamDev <114930907+TienPhamDev@users.noreply.github.com>
2024-01-15 04:20:15 +0000

    comit

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ app = Flask(__name__)
 @app.route('/') # this is the home page route
 def index():
   #code
-  user_logged_in = True
-  return render_template('basic.html',user_logged_in = user_logged_in)
+  return render_template('basic.html')
+
+
+@app.route('/puppy/<name>')
+def puppy(name):
+  return render_template('puppy.html', name=name)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=81,debug=True)
