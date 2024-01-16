84033c0643506498ea9c81c405f69a6becdd6fac
TienPhamDev <manhtienbn18891@gmail.com>
2024-01-15 22:17:25 -0500

    end testing in flask

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ app = Flask(__name__)
 @app.route('/') # this is the home page route
 def index():
   #code
-  return render_template('basic.html')
+  return render_template('base.html')
 
+@app.route('/home.html')
+def home():
+  return render_template('home.html')
 
 @app.route('/puppy/<name>')
 def puppy(name):
   return render_template('puppy.html', name=name)
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=81,debug=True)
+  app.run(debug=True)
